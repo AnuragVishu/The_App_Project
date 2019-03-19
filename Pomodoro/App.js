87e5524d898b157161f5ae9b098e470ff0885c1e
AnuragVishu <anurag.vishwakarma9@gmail.com>
2019-03-19 19:15:51 +0530

    Updated

--- Pomodoro/App.js
+++ Pomodoro/App.js
@@ -2,28 +2,50 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {RkButton, RkCard} from 'react-native-ui-kitten';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import ProgressCircle from 'react-native-progress-circle'
 
-
-export default class App extends React.Component {
+export default class App  extends React.Component {
   render() {
+
+    var timeCounterArray = [];
+    for(let i = 0; i < 240; i++){
+      timeCounterArray.push({i})
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Pomodoro Timer</Text>
-        <RkButton rkType='small' style={[styles.custombutton]}>Custom</RkButton>
-        <RkCard rkType='shadowed' style={styles.progressbar}>
-          <AnimatedCircularProgress 
-  size={200}
-  width={9}
-  fill={100}
-  tintColor="#00e0ff"
-  onAnimationComplete={() => console.log('onAnimationComplete')}
-  backgroundColor="#3d5875" />
+        <Text style={styles.text}>Pomodoro</Text>
+        <RkButton rkType='small' style={[styles.custombutton]} onPress={this._showDateTimePicker}>Custom </RkButton>
+        <RkCard rkType='shadowed' style={[styles.progressbar, {padding: 12, margin: 15, backgroundColor: '#f1f1f1',}]}>
+        <ProgressCircle
+            percent={30}
+            radius={100}
+            borderWidth={8}
+            color="#000000"
+            shadowColor="#999"
+            bgColor="#b30000"
+        >
+            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
+        </ProgressCircle>
         </RkCard>
+        
        <View style={[styles.belowbuttonview]}>
        <RkButton rkType='small' style={{margin: 15,borderRadius: 20,}}>20 min</RkButton>
        <RkButton rkType='small' style={{margin: 15,borderRadius: 20}}>30 min</RkButton>
        <RkButton rkType='small' style={{margin: 15,borderRadius: 20}}>45 min</RkButton>
        </View>
+       {/* <ProgressCircle
+            percent={30}
+            radius={50}
+            borderWidth={8}
+            color="#3399FF"
+            shadowColor="#999"
+            bgColor="#fff"
+        >
+            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
+        </ProgressCircle> */}
+
+            
       </View>
     );
   }
@@ -50,7 +72,7 @@ const styles = StyleSheet.create({
   custombutton: {
     top: '40%',
     left: '40%',
-    backgroundColor: 'red',
+    backgroundColor: '#ff3333',
   },
   progressbar: {
     top: '55%',
@@ -64,6 +86,4 @@ const styles = StyleSheet.create({
     top: '80%',
     justifyContent: 'center',
   },
-
-
 });
