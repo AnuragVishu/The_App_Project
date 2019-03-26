1680505836d7980b3d8ccfed7aeb46d3e574134d
AnuragVishu <anurag.vishwakarma9@gmail.com>
2019-03-26 19:07:08 +0530

    Gear Icon Added

--- Pomodoro/App.js
+++ Pomodoro/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import {RkButton, RkCard} from 'react-native-ui-kitten';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
-import ProgressCircle from 'react-native-progress-circle'
+import ProgressCircle from 'react-native-progress-circle';
+import { Button } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class App  extends React.Component {
   render() {
@@ -15,37 +17,33 @@ export default class App  extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Pomodoro</Text>
+        
+     <View style={{flex: 0, flexDirection:'column', alignItems: 'flex-end', right: '2%', top: '1%'}}>
+      <Icon name="gear" size={32}  color="#3f51b5"/>
+     </View>
+        
+        
         <RkButton rkType='small' style={[styles.custombutton]} onPress={this._showDateTimePicker}>Custom </RkButton>
-        <RkCard rkType='shadowed' style={[styles.progressbar, {padding: 12, margin: 15, backgroundColor: '#f1f1f1',}]}>
+        <RkCard rkType='shadowed' style={[styles.progressbar, {padding:'3%',margin:"2%", backgroundColor: 'white',}]}>
+        
         <ProgressCircle
             percent={30}
             radius={100}
             borderWidth={8}
-            color="#000000"
-            shadowColor="#999"
-            bgColor="#b30000"
+            color="#f44b42"
+            shadowColor="white"
+            bgColor="#fff"
         >
-            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
+            <Text style={{ fontSize: 18, color: 'red', fontWeight:'bold' }}>{'30%'}</Text>
         </ProgressCircle>
+
         </RkCard>
         
        <View style={[styles.belowbuttonview]}>
-       <RkButton rkType='small' style={{margin: 15,borderRadius: 20,}}>20 min</RkButton>
-       <RkButton rkType='small' style={{margin: 15,borderRadius: 20}}>30 min</RkButton>
-       <RkButton rkType='small' style={{margin: 15,borderRadius: 20}}>45 min</RkButton>
+       <RkButton rkType='small' style={{margin: 11,borderRadius: 20,backgroundColor: "#3f51b5"}}>20 min</RkButton>
+       <RkButton rkType='small' style={{margin: 11,borderRadius: 20,backgroundColor: "#3f51b5"}}>30 min</RkButton>
+       <RkButton rkType='small' style={{margin: 11,borderRadius: 20,backgroundColor: "#3f51b5"}}>45 min</RkButton>
        </View>
-       {/* <ProgressCircle
-            percent={30}
-            radius={50}
-            borderWidth={8}
-            color="#3399FF"
-            shadowColor="#999"
-            bgColor="#fff"
-        >
-            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
-        </ProgressCircle> */}
-
-            
       </View>
     );
   }
@@ -58,32 +56,31 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     backgroundColor: 'white'
   },
+
   text: {
     textAlign: 'center',
     justifyContent: 'center',
     top: '5%',
     fontSize: 25,
+    fontWeight: 'bold',
   },
-  // clock: {
-  //   fontSize: 10,
-  //   top: '45%',
-  //   flexWrap: 'nowrap',
-  // },
+
   custombutton: {
-    top: '40%',
+    top: '25%',
     left: '40%',
     backgroundColor: '#ff3333',
   },
+
   progressbar: {
-    top: '55%',
+    top: '45%',
     flexWrap: 'wrap',
     justifyContent: 'center',
     flexDirection: 'row',
-    //backgroundColor: 'red',
   },
+
   belowbuttonview: {
     flexDirection: 'row',
-    top: '80%',
+    top: '70%',
     justifyContent: 'center',
   },
 });
